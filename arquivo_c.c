1cc17b5388247cec1e94522c73a0883d06675913
gabriel <gabrielandrade1504@gmail.com>
2019-08-21 17:23:55 -0300

    commit realizado na nova branch

---
+++ arquivo_c.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+int idade;
+
+printf("Digite sua idade: ");
+scanf("%d",&idade);
+}
+
---
+++ outroarquivo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+int idade;
+
+printf("Digite sua idade: ");
+scanf("%d",&idade);
+if(idade>=18){
+	printf("Voce é adulto");
+}
+else{
+	printf("Voce nao é adulto ainda")
+}
+}
